49624d5e7a0ccf9ba225b99a5746d126e9829328
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2021-12-20 16:28:00 -0500

    Default to fast

--- types.h
+++ types.h
@@ -3,15 +3,15 @@
 
 #include <stdint.h>
 
-typedef u8    uint8_t
-typedef u16   uint16_t
-typedef u32   uint32_t
-typedef u64   uint64_t
+typedef u8_t    uint8_t
+typedef u16_t   uint16_t
+typedef u32_t   uint32_t
+typedef u64_t   uint64_t
 
-typedef i8    int8_t
-typedef i16   int16_t
-typedef i32   int32_t
-typedef i64   int64_t
+typedef i8_t    int8_t
+typedef i16_t   int16_t
+typedef i32_t   int32_t
+typedef i64_t   int64_t
 
 typedef f32   float
 typedef f64   double
@@ -20,30 +20,30 @@ typedef f64   double
 
 #include <stdatomic.h>
 
-typedef u8_f  atomic_uint_fast8_t
-typedef u16_f atomic_uint_fast16_t
-typedef u32_f atomic_uint_fast32_t
-typedef u64_f atomic_uint_fast64_t
+typedef u8  atomic_uint_fast8_t
+typedef u16 atomic_uint_fast16_t
+typedef u32 atomic_uint_fast32_t
+typedef u64 atomic_uint_fast64_t
 
-typedef i8_f  atomic_int_fast8_t
-typedef i16_f atomic_int_fast16_t
-typedef i32_f atomic_int_fast32_t
-typedef i64_f atomic_int_fast64_t
+typedef i8  atomic_int_fast8_t
+typedef i16 atomic_int_fast16_t
+typedef i32 atomic_int_fast32_t
+typedef i64 atomic_int_fast64_t
 
 #else
 
-typedef u8_f  u8
-typedef u16_f u16
-typedef u32_f u32
-typedef u64_f u64
+typedef u8 u8_t
+typedef u16 u16_t
+typedef u32 u32_t
+typedef u64 u64_t
 
-typedef i8_f  i8
-typedef i16_f i16
-typedef i32_f i32
-typedef i64_f i64
+typedef i8  i8_t
+typedef i16 i16_t
+typedef i32 i32_t
+typedef i64 i64_t
 
 #endif //__STD_NO_THREADS__
 
-tyepdef b8 u8_f
+tyepdef b8 u8
 
 #endif //ENGINE_TYPES_H
