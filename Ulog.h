c01d3ef9de6722f9716a33d7f5935985fe9331e7
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2021-12-21 13:13:08 -0500

    Update Ulog.h

--- Ulog.h
+++ Ulog.h
@@ -2,13 +2,14 @@
 #define UNSALTED_ENGINE_ULOG_H
 
 #include <stdio.h>
+
 typedef static const enum _log_level {
     DEBUG,
     INFO,
     WARN,
     ERROR,
     FATAL
-} log_level
+} log_level;
 
 void set_log_stream(FILE *);
 void log(const char *, log_level);
