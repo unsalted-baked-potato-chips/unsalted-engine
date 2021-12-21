7979b35bb402d8200a8c431ae2cc0d8bad3aaa24
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2021-12-21 11:58:30 -0500

    Create ulog.c

---
+++ ulog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "ulog.h"
+
+static File *log_stream = stderr;
+
+static const char prefix[][] = {
+  [DEBUG] = "DEBUG",
+  [INFO] = "INFO",
+  [WARN] = "WARN",
+  [ERROR] = "ERROR",
+  [FATAL] = "FATAL"
+}
+
+void set_ulog_stream(FILE * stream){
+  log_stream = stream;
+}
+
+void log(const char * desc, log_level level){
+  fprintf(log_stream, "%s: %s.\n", prefix[level], desc);
+}
