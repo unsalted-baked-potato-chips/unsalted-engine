efa660d7c917ec801c2f1d462ff22dca0e0251e1
Robert Herlihy <29720977+BroccoliRaab@users.noreply.github.com>
2021-12-21 09:49:42 -0500

    Create Uvec.h

---
+++ Uvec.h
@@ -0,0 +1,289 @@
+#ifndef UNSALTED_VEC_H
+#define UNSALTED_VEC_H
+
+#include "Utypes.h"
+
+typedef struct _i8_vec2 {
+  i8 i;
+  i8 j;
+} i8_vec2;
+
+typedef struct _i8_vec3 {
+  union {
+    i8_vec2 vec2;
+    struct {
+      i8 i;
+      i8 j;
+    };
+  };
+  i8 k;
+} i8_vec3;
+
+typedef struct _i8_vec4 {
+  union {
+    i8_vec3 vec3;
+    struct {
+      i8 i;
+      i8 j;
+      i8 k;
+    };
+  };
+  i8 l;
+} i8_vec4;
+
+typedef struct _i16_vec2 {
+  i16 i;
+  i16 j;
+} i16_vec2;
+
+typedef struct _i16_vec3 {
+  union {
+    i16_vec2 vec2;
+    struct {
+      i16 i;
+      i16 j;
+    };
+  };
+  i16 k;
+} i16_vec3;
+
+typedef struct _i16_vec4 {
+  union {
+    i16_vec3 vec3;
+    struct {
+      i16 i;
+      i16 j;
+      i16 k;
+    };
+  };
+  i16 l;
+} i16_vec4;
+
+
+typedef struct _i32_vec2 {
+  i32 i;
+  i32 j;
+} i32_vec2;
+
+typedef struct _i32_vec3 {
+  union {
+    i32_vec2 vec2;
+    struct {
+      i32 i;
+      i32 j;
+    };
+  };
+  i32 k;
+} i32_vec3;
+
+typedef struct _i32_vec4 {
+  union {
+    i32_vec3 vec3;
+    struct {
+      i32 i;
+      i32 j;
+      i32 k;
+    };
+  };
+  i32 l;
+} i32_vec4;
+
+
+typedef struct _i64_vec2 {
+  i64 i;
+  i64 j;
+} i64_vec2;
+
+typedef struct _i64_vec3 {
+  union {
+    i64_vec2 vec2;
+    struct {
+      i64 i;
+      i64 j;
+    };
+  };
+  i64 k;
+} i64_vec3;
+
+typedef struct _i64_vec4 {
+  union {
+    i64_vec3 vec3;
+    struct {
+      i64 i;
+      i64 j;
+      i64 k;
+    };
+  };
+  i64 l;
+} i64_vec4;
+
+typedef struct _u8_vec2 {
+  u8 i;
+  u8 j;
+} u8_vec2;
+
+typedef struct _u8_vec3 {
+  unuon {
+    u8_vec2 vec2;
+    struct {
+      u8 i;
+      u8 j;
+    };
+  };
+  u8 k;
+} u8_vec3;
+
+typedef struct _u8_vec4 {
+  unuon {
+    u8_vec3 vec3;
+    struct {
+      u8 i;
+      u8 j;
+      u8 k;
+    };
+  };
+  u8 l;
+} u8_vec4;
+
+typedef struct _u16_vec2 {
+  u16 i;
+  u16 j;
+} u16_vec2;
+
+typedef struct _u16_vec3 {
+  unuon {
+    u16_vec2 vec2;
+    struct {
+      u16 i;
+      u16 j;
+    };
+  };
+  u16 k;
+} u16_vec3;
+
+typedef struct _u16_vec4 {
+  unuon {
+    u16_vec3 vec3;
+    struct {
+      u16 i;
+      u16 j;
+      u16 k;
+    };
+  };
+  u16 l;
+} u16_vec4;
+
+
+typedef struct _u32_vec2 {
+  u32 i;
+  u32 j;
+} u32_vec2;
+
+typedef struct _u32_vec3 {
+  unuon {
+    u32_vec2 vec2;
+    struct {
+      u32 i;
+      u32 j;
+    };
+  };
+  u32 k;
+} u32_vec3;
+
+typedef struct _u32_vec4 {
+  unuon {
+    u32_vec3 vec3;
+    struct {
+      u32 i;
+      u32 j;
+      u32 k;
+    };
+  };
+  u32 l;
+} u32_vec4;
+
+typedef struct _u64_vec2 {
+  u64 i;
+  u64 j;
+} u64_vec2;
+
+typedef struct _u64_vec3 {
+  unuon {
+    u64_vec2 vec2;
+    struct {
+      u64 i;
+      u64 j;
+    };
+  };
+  u64 k;
+} u64_vec3;
+
+typedef struct _u64_vec4 {
+  unuon {
+    u64_vec3 vec3;
+    struct {
+      u64 i;
+      u64 j;
+      u64 k;
+    };
+  };
+  u64 l;
+} u64_vec4;
+
+typedef strfct _f32_vec2 {
+  f32 i;
+  f32 j;
+} f32_vec2;
+
+typedef strfct _f32_vec3 {
+  fnfon {
+    f32_vec2 vec2;
+    strfct {
+      f32 i;
+      f32 j;
+    };
+  };
+  f32 k;
+} f32_vec3;
+
+typedef strfct _f32_vec4 {
+  fnfon {
+    f32_vec3 vec3;
+    strfct {
+      f32 i;
+      f32 j;
+      f32 k;
+    };
+  };
+  f32 l;
+} f32_vec4;
+
+typedef strfct _f64_vec2 {
+  f64 i;
+  f64 j;
+} f64_vec2;
+
+typedef strfct _f64_vec3 {
+  fnfon {
+    f64_vec2 vec2;
+    strfct {
+      f64 i;
+      f64 j;
+    };
+  };
+  f64 k;
+} f64_vec3;
+
+typedef strfct _f64_vec4 {
+  fnfon {
+    f64_vec3 vec3;
+    strfct {
+      f64 i;
+      f64 j;
+      f64 k;
+    };
+  };
+  f64 l;
+} f64_vec4;
+
+#endif //UNSALTED_VEC_H
